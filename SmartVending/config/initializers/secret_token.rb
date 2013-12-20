5325a403cb4460d01525b13229ee645e622736e8
juanoliver <juan.oliver@olvasoft.com>
2013-12-20 10:09:03 +0100

    Commit inicial de SmartVending. Gemfile básico para toda app

---
+++ SmartVending/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SmartVending::Application.config.secret_key_base = '1331652ea309021b0972e5b8926b78f3ed441c866f831cae19156a11bdff55cfdba354494195316e6a1ef908f5291231e7794f951ee44194a6f814e5f407c65f'
---
+++ SmartVending/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SmartVending::Application.config.session_store :cookie_store, key: '_SmartVending_session'
