2772f1f132f201401354738fce1b4d5189dfa5a5
juanoliver <juan.oliver@olvasoft.com>
2013-12-24 17:00:06 +0100

    new layout with icons

--- SmartVending/config/application.rb
+++ SmartVending/config/application.rb
@@ -19,7 +19,11 @@ module SmartVending
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
+    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif,
+                             "fontawesome-webfont.ttf",
+                             "fontawesome-webfont.eot",
+                             "fontawesome-webfont.svg",
+                             "fontawesome-webfont.woff")
     config.assets.version = '1.0'
     config.assets.initialize_on_precompile = false
   end
